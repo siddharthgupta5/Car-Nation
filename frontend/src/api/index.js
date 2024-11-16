a91b85c7da715b19a39e34fa6441f37198733601
SIDDHARTHGUPTA <contactsid14@gmail.com>
2024-11-16 19:04:58 +0530

    backend deployed

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "https://carmanagementapplication.onrender.com/api";
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://car-nation-nik8.onrender.com";
+// const API_BASE_URL = "http://localhost:5000/api";
 
 export const api = {
 
